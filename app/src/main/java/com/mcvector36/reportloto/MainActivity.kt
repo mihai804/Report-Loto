c5984a9475c66fdd1cc19dd8e28e5aefa66cceda
mcvector36 <constantinmihai804@gmail.com>
2024-02-07 03:07:51 +0200

    stabil4

--- app/src/main/java/com/mcvector36/reportloto/MainActivity.kt
+++ app/src/main/java/com/mcvector36/reportloto/MainActivity.kt
@@ -5,18 +5,30 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-
+import com.mcvector36.reportloto.ui.theme.ReportLotoTheme
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MainScreen()
+            ReportLotoTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    MainScreen()
+                }
+            }
         }
 
     }
