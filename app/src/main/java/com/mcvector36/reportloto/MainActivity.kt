fe987be5ff8752caf9630b342b15495b15fd7963
tanta9 <zafiuconstanta9@gmail.com>
2024-03-09 13:10:08 +0200

    V3.4.2-test1
    - eliminare reclame

--- app/src/main/java/com/mcvector36/reportloto/MainActivity.kt
+++ app/src/main/java/com/mcvector36/reportloto/MainActivity.kt
@@ -4,15 +4,10 @@ import android.os.Bundle
 import androidx.activity.addCallback
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.constraintlayout.widget.ConstraintSet
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.gms.ads.AdListener
-import com.google.android.gms.ads.AdRequest
-import com.google.android.gms.ads.AdView
-import com.google.android.gms.ads.LoadAdError
 import com.google.android.gms.ads.MobileAds
 import com.mcvector36.reportloto.databinding.ActivityMainBinding
 
@@ -20,7 +15,6 @@ import com.mcvector36.reportloto.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    private lateinit var mAdView : AdView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,54 +37,8 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
-        incarcaReclama()
 
-        onBackPressedDispatcher.addCallback(this) { finish()}
-
-    }
-
-    fun incarcaReclama(){
-        mAdView = findViewById(R.id.adView) // reclama
-        val adRequest = AdRequest.Builder().build() // reclama
-        mAdView.loadAd(adRequest) //reclama
-
-        val constraintSet = ConstraintSet()
-        constraintSet.clone(binding.root)
-
-        mAdView.adListener = object: AdListener() {
-            override fun onAdClicked() {
-                // Code to be executed when the user clicks on an ad.
-            }
-
-            override fun onAdClosed() {
-                // Code to be executed when the user is about to return
-                // to the app after tapping on an ad.
-            }
-
-            override fun onAdFailedToLoad(adError : LoadAdError) {
-                // Înlocuiți adView cu parent
-                constraintSet.connect(R.id.nav_host_fragment_activity_main, ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP)
-
-                // Aplicați modificările
-                constraintSet.applyTo(binding.root)
-            }
-
-            override fun onAdImpression() {
-                // Code to be executed when an impression is recorded
-                // for an ad.
-            }
-
-            override fun onAdLoaded() {
-                constraintSet.connect(R.id.nav_host_fragment_activity_main, ConstraintSet.TOP, R.id.adView, ConstraintSet.BOTTOM)
-                constraintSet.applyTo(binding.root)
-            }
-
-            override fun onAdOpened() {
-                // Code to be executed when an ad opens an overlay that
-                // covers the screen.
-            }
-        }
+        onBackPressedDispatcher.addCallback(this) { finish() }
 
     }
-
 }
